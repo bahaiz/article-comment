8643165391da2fe334a75f609799d2f01a642b44
gitbh <bh.zmrlglu@gmail.com>
2021-11-10 04:32:31 +0000

    articles listed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://react-yazi-yorum.herokuapp.com/posts")
+      .then((response) => {
+        setArticles(response.data);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header></header>
+      <div className="ui raised very padded text container segment">
+        <div className="ui relaxed divided list">
+          {articles.map((article) => {
+            return (
+              <div key={article.id} className="item">
+                <i className="large github middle aligned icon"></i>
+                <div className="content">
+                  <a className="header">{article.title}</a>
+                  <div className="description">{article.created_at}</div>
+                </div>
+              </div>
+            );
+          })}
+
+          
+        </div>
+      </div>
     </div>
   );
 }
